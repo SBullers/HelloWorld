6cc1b75c6e856b65c1a173582984dd9f9504c81e
Sara Azevedo Machado <samszie@MacBook-Pro.local>
2022-03-29 14:40:37 -0700

    Hello world setup

---
+++ HelloWorld/HelloWorldProj.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string name;
+    cout << "Enter your name: \n";
+    cin >> name;
+    cout << "Say Hello World, " << name << "!" << endl;
+    return 0;
+    
+}
